0f229269cfa7d31bc6274c24907a530de5cf8769
Zach B <zachbush96+github@gmail.com>
2024-11-22 10:21:07 -0500

    Initial Commit

---
+++ SecPlusLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecPlusLearning
+//
+//  Created by Zach Bush on 11/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecPlusLearning/SecPlusLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecPlusLearningApp.swift
+//  SecPlusLearning
+//
+//  Created by Zach Bush on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SecPlusLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
